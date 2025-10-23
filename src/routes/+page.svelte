8c3e1d1961ba998224bb3c063b3303a86d47874e
Rodolfo valtierra <rivd@outlook.es>
2025-10-22 19:13:12 -0600

    adding aws amplify static

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script>
 	import Hero from '../Main/Hero.svelte'
 	import Section2 from '../Main/Section2.svelte'
+	
 </script>
 
 <Hero/>
--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,21 @@
 import adapter from '@sveltejs/adapter-static';
-
+import {vitePreprocess} from '@sveltejs/vite-plugin-svelte'
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter(),
-		prerender: {
-			default: true
+		adapter: adapter({
+			
+			fallback: '200.html' // may differ from host to host
+		
+		}),
+		paths: {
+			base: ''
 		}
-	}
+	},
+	preprocess: vitePreprocess(),
 };
 
 export default config;
