86b434f305744beb922231928f3a4b9c34d6df06
Rodolfo valtierra <rivd@outlook.es>
2025-10-28 13:06:31 -0600

    fix mainPage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script>
-	import Hero from '$mainPage/Hero.svelte';
-	import Section2 from '$mainPage/Section2.svelte';
-	import Services from '$mainPage/Services.svelte';
+	import Hero from '$main/Hero.svelte';
+	import Section2 from '$main/Section2.svelte';
+	import Services from '$main/Services.svelte';
 </script>
 
 <Hero/>
