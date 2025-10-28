0461357171b52fddcce2c4d8fe79bceaef6ff533
Rodolfo valtierra <rivd@outlook.es>
2025-10-28 08:45:53 -0600

    changing root directory in vite.config

--- svelte.config.js
+++ svelte.config.js
@@ -15,7 +15,7 @@ const config = {
       handleHttpError: 'warn'
 		},
 		alias: {
-			$main: path.resolve('./main'),
+			$main: path.resolve('./src/main'),
       $components: path.resolve('./src/components'),
 			$lib: path.resolve('./src/lib'),		
 			$utils: path.resolve('./src/utils'),		
