06a3107762da13f6217443b9e0ab49902fbf1de9
Rodolfo valtierra <rivd@outlook.es>
2025-10-27 22:45:58 -0600

    reverse

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,7 @@
 import adapter from '@sveltejs/adapter-static';
 import {vitePreprocess} from '@sveltejs/vite-plugin-svelte'
 import path from 'path'
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
